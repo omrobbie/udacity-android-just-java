d1b2fe08a26a51fdcddce2b491776ca2d2b754a5
omrobbie <putu.roby@yahoo.com>
2017-05-10 14:43:55 +0800

    tambahkan tombol + dan - untuk quantity

--- app/src/main/java/com/omrobbie/justjava/MainActivity.java
+++ app/src/main/java/com/omrobbie/justjava/MainActivity.java
@@ -31,9 +31,25 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity = 5;
+        int quantity = 2;
         display(quantity);
         displayPrice(quantity * 5);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void increment(View view) {
+        int quantity = 3;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void decrement(View view) {
+        int quantity = 1;
+        display(quantity);
     }
 
     /**
